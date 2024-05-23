03eec87993a74f84302e2fc7bd41a8f5bd0f40c2
khushiram <khushiramrajliwal@gmail.com>
2024-05-23 17:54:02 +0530

    added

--- controllers/otpControllers.js
+++ controllers/otpControllers.js
@@ -5,13 +5,12 @@ import otpModel from "../models/otpModel.js"
 
 export async function sendOtp (req,res) {
   try {
-   const  {number}=req.body
     const otp=randomInt(1000,10000)
+    const number="+919729892769"
     const hash="0U5lmlXfi0d"
     const otpstored=await otpModel.create({number:number,otp:otp})
     const smsTemplate=`Your verification code is ${otp}. Please use this code to verify your account. This code will expire in 5 minutes.\n\n #${hash}`
     await sendSMS(number,smsTemplate);
-    console.log("herer")
 res.status(200).send({message:"message sent sucessfully"})
   } catch (error) {
     res.status(500).send({message:"internal server error"})
